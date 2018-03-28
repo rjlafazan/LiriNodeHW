0b9243fa80fdc06c03041b1b94dbe9a1b333a1e3
Ryan <rjlafazan@gmail.com>
2018-03-28 15:02:10 -0700

    spotify require working

--- liri.js
+++ liri.js
@@ -6,15 +6,18 @@ var keys = require("./keys.js");
 
 // calls necessary apis
 var Twitter = require("twitter");
+var Spotify = require("node-spotify-api");
 
 //key access
 var client = new Twitter(keys.twitter);
+var spotify = new Spotify(keys.spotify);
 
 //display  tweets
 if (process.argv[2] == "my-tweets") {
   function displayTweets() {
     //npm twitter documentation to display tweets
     var params = { screen_name: "mountainfit24", count: 20, include_rts: true };
+
     client.get("statuses/user_timeline", params, function(
       error,
       tweets,
@@ -23,12 +26,19 @@ if (process.argv[2] == "my-tweets") {
       if (error) {
         throw error;
       } else {
-        // for (var i = 0; i < tweet.length; i++) {
-        //   console.log(tweet[i].created_at + "\n" + tweet[i].text + "\n");
-        // }
-        console.log(tweets);
+        for (var i = 0; i < tweets.length; i++) {
+          console.log(tweets[i].created_at + " " + tweets[i].text + " ");
+        }
+        // console.log(tweets);
       }
     });
   }
   displayTweets();
+}
+
+if (process.argv[2] == "spotify-this-song") {
+  //doing stuff to display song information
+  function displaySpotifyData(params) {
+    var songTitle = process.argv[3];
+  }
 }
